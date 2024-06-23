a5ed87ea51a7cfe69dd9c2fc8a962a643abd7b0c
Smritibajaj <my@pers.on.al>
2024-06-24 04:06:02 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,6 @@
 // vite.config.ts
 import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
-const excludeFiles = (regex: RegExp) => {
-  return {
-    name: "exclude-files",
-    load(id: string) {
-      if (regex.test(id)) {
-        return { code: "" }; // Return an empty module
-      }
-    },
-  };
-};
 
 export default defineConfig({
   plugins: [react()],
@@ -19,11 +9,5 @@ export default defineConfig({
   //   environment: "jsdom",
   //   setupFiles: "./src/setupTests.ts",
   // },
-  build: {
-    rollupOptions: {
-      input: {
-        main: "src/main.tsx", // Adjust according to your entry point
-      },
-    },
-  },
+  
 });
